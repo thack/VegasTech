481a7fcb8977bdd1869c84f372f805e12cf1533a
Terence Carroll <git@thack.com>
2014-08-16 16:08:04 -0700

    Add more to Individual.

--- people/people-controller.js
+++ people/people-controller.js
@@ -2,5 +2,5 @@ angular.module('people', ['api']);
 
 angular.module('people').controller('PeopleController', function($scope, API) {
     $scope.people = API.People.query();
-    $scope.individual = API.People.get({ id: 6 });
+    $scope.individual = API.People.get({ id: 9 });
 });
